a6099a39dcc794325d6f440b9d261875f93e5c08
mbc5674 <mbc5674@psu.edu>
2024-06-12 13:25:57 -0400

    Header and navbar component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BackgroundComponent } from '../library/background/background.component';
+import { HeaderComponent } from '../library/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BackgroundComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
